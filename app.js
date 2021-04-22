b5c6a85b74a61b68b90a2f2adbed14598ccf1fee
GermanDG6 <germandg6@gmail.com>
2021-04-22 09:04:29 +0100

    Connect to MongoAtlas

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const User = require('./models/User.model')
 
 //Configuración DB
 mongoose
-  .connect('mongodb://localhost/passport-example', {
+  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.la1hq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
   })
